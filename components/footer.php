cc1f624b9ee03f2c5fd7b1da9b2fc46f77cbb5ed
Sudarshan Tripathi <157094931+Flash9933@users.noreply.github.com>
2024-07-02 10:46:17 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9022388185"><i class="fas fa-phone"></i> +919022388185</a>
+         <a href="tel:9792986601"><i class="fas fa-phone"></i> +919792986601</a>
+         <a href="mailto:sudarshantripathi00@gmail.com"><i class="fas fa-envelope"></i> phoneOasis@gmail.com</a>
+         <a href="https://maps.app.goo.gl/541EX63xKKXjciGf9"><i class="fas fa-map-marker-alt"></i> Mumbai,Maharashtra </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/share/Yp1VCKnGPWcgzdoB/?mibextid=qi2Omg" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://github.com/Flash9933" target="_blank"><i class="fab fa-github"></i>Github</a>
+         <a href="https://www.instagram.com/sudarshan_9933?igsh=MWI4N2dmaXR5cW15NQ==" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://in.linkedin.com/in/sudarshan-tripathi-6412202bb" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> By <span>Phone Oasis</span> | all rights reserved!</div>
+
+</footer>
